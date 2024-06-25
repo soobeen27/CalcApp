c7ec908544d5d43fbd1dc28f98630ab59e949085
soobeen27 <youdew27@gmail.com>
2024-06-26 00:05:47 +0900

    Initial Commit

---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by Soo Jang on 6/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
